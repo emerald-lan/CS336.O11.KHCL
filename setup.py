4b42f9040cb3e8e64ffcda791b60994d6eca2a9b
platinumhb <21520618@gm.uit.edu.vn>
2024-02-02 01:54:02 +0700

    Important reformat all, Init search.py; Update md, setup, requirements, app.py -> Total process 85%

--- setup.py
+++ setup.py
@@ -1,46 +1,46 @@
-import streamlit as st
 import os
 import gdown
-import rarfile
-import clip
-import torch
-from PIL import Image
+import zipfile
 
 def download_files():
     dataset_name = "splited_fashionIQ"  # Replace with your desired dataset folder name
     finetune_name = "finetuned_RN50.pt"
-    folder_path = "./data"
-    dataset_url = 'https://drive.google.com/file/d/1B_ahBg0B7nUJE2dYROxdpXpqicMfcyu7/view?usp=sharing'
+    
+    dataset_url = 'https://drive.google.com/file/d/1ueW5JoJA-nCWEENeElq-kg36l9GHrZyZ/view?usp=sharing'
     finetune_url = 'https://drive.google.com/file/d/1K8QuwOpoufjx9Y1_56vrk2ynd6Y3FjZW/view'
-    rar_path = os.path.join(folder_path, "splited_fashionIQ.rar")
+    
+    folder_path = "./"
+    zip_path = os.path.join(folder_path, "splited_fashionIQ_val_test.zip")
     extract_path = os.path.join(folder_path, dataset_name)
     finetune_path = os.path.join(folder_path, finetune_name)
-
-    st.write("Loading files...")
+    
+    print("Loading files...")
     # Check if the folder already exists
-    if not os.path.exists(finetune_path):     
+    if not os.path.exists(finetune_path):   
+        print("Downloading the fine-tuned model. Please wait...")  
         gdown.download(finetune_url, finetune_path, quiet=False, fuzzy=True)
     
     if not os.path.exists(extract_path):     
-        print("Downloading the dataset. Please wait...")
-        gdown.download(dataset_url, rar_path, quiet=False, fuzzy=True)
+        if not os.path.exists(zip_path):
+            print("Downloading the dataset. Please wait...")
+            gdown.download(dataset_url, zip_path, quiet=False, fuzzy=True)
+        else:
+            print("Loading the splited_fashionIQ_val_test.zip")
+            
+        print("Extracting... Path: " + extract_path)
+        # Extract the downloaded ZIP file
+        with zipfile.ZipFile(zip_path,"r") as zip_ref:
+            zip_ref.extractall(folder_path)
 
-        # Extract the downloaded RAR file
-        with rarfile.RarFile(rar_path, 'r') as rf:
-            rf.extractall(extract_path)
+        # Remove the downloaded ZIP file
+        os.remove(zip_path)
 
-        # Remove the downloaded RAR file
-        os.remove(rar_path)
-
-        print(f"Extraction completed. Path = {folder_path}")
+        print(f"Extraction completed. =")
         
-    st.write("Everything is downloaded!")
+    print("Everything is downloaded!")
     
 def main():
     download_files()
-    if not os.path.exists("./data/features"):
-        os.makedirs("./data/features")
 
 if __name__ == "__main__":
     main()
-
