a145f5eadfd9c27ede113397bcca01c1da546db7
Thanh Lan <emeraldlan1105@gmail.com>
2024-02-05 00:31:50 +0700

    update download_resources.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 DATASET_URL = 'https://drive.google.com/file/d/1ueW5JoJA-nCWEENeElq-kg36l9GHrZyZ/view?usp=sharing'
 MODEL_URL = 'https://drive.google.com/file/d/1K8QuwOpoufjx9Y1_56vrk2ynd6Y3FjZW/view?usp=sharing'
+QUERY_URL = 'https://drive.google.com/file/d/1GPEk45RDopjWEu7I0W-W9-LADeFxP86H/view?usp=drive_link'
 
 PROJECT_DIR = Path(__file__).parent.absolute()
 DATA_DIR = PROJECT_DIR / "data"
--- src/download_resources.py
+++ src/download_resources.py
@@ -3,8 +3,25 @@ import gdown
 import os
 import shutil
 import zipfile
+import json
 from config import *
 
+
+def download_process_query(query_url: str, query_file: Path) -> None:
+    gdown.download(query_url, str(query_file), quiet=False, fuzzy=True)
+
+    with open(query_file, 'r') as file:
+        data = json.load(file)
+
+    targets = [obj['target'] for obj in data]
+    filtered_gallery = [obj for obj in data if obj['candidate'] not in targets]
+    
+    output_json_path = DATA_DIR / "filtered_gallery.json"
+
+    with open(output_json_path, 'w') as output_file:
+        json.dump(filtered_gallery, output_file, indent=2)
+
+    print("Query file downloaded and processed")
 
 def download_clip(model_url: str, model_file: Path) -> None:
     if not RESOURCES_DIR.exists():
@@ -34,6 +51,9 @@ def main():
 
     dataset_file = DATA_DIR / "dataset.zip"
     download_extract_dataset(DATASET_URL, dataset_file)
+
+    query_file = DATA_DIR / "text_query.json"
+    download_process_query(QUERY_URL, query_file)
 
 if __name__ == "__main__":
     main()
