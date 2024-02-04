6bdf3cfb1920a51b13eb362644ac9de49a40936d
Thanh Lan <emeraldlan1105@gmail.com>
2024-02-05 00:32:40 +0700

    update download links

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-DATASET_URL = 'https://drive.google.com/file/d/1ueW5JoJA-nCWEENeElq-kg36l9GHrZyZ/view?usp=sharing'
+DATASET_URL = 'https://drive.google.com/file/d/1D9aydHJeKCv4ZU8qH67ZZ2b7uoMc7uie/view?usp=sharing'
 MODEL_URL = 'https://drive.google.com/file/d/1K8QuwOpoufjx9Y1_56vrk2ynd6Y3FjZW/view?usp=sharing'
-QUERY_URL = 'https://drive.google.com/file/d/1GPEk45RDopjWEu7I0W-W9-LADeFxP86H/view?usp=drive_link'
+QUERY_URL = 'https://drive.google.com/file/d/1wt8I5zeHysjQNuLYJLBGHn39g_F9LE5S/view?usp=sharing'
 
 PROJECT_DIR = Path(__file__).parent.absolute()
 DATA_DIR = PROJECT_DIR / "data"
